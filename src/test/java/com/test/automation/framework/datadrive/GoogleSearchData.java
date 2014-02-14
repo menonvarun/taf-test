57ef266a17af258ee85053427f0adfbc50c2d387
Varun Menon <mr.varun.menon@gmail.com>
2014-02-14 11:46:02 +0530

    Addign taf related tests

---
+++ src/test/java/com/test/automation/framework/datadrive/GoogleSearchData.java
@@ -0,0 +1,9 @@
+package com.test.automation.framework.datadrive;
+
+public class GoogleSearchData {
+	
+	public String searchtext;
+	
+	public String titleToWait;
+
+}
---
+++ src/test/java/com/test/automation/framework/datadrive/TestDataClass.java
@@ -0,0 +1,14 @@
+package com.test.automation.framework.datadrive;
+
+public class TestDataClass {
+	public String stringValue;
+	public int integerValue;
+	public byte byteValue;
+	public float floatValue;
+	
+	@Override
+	public String toString() {
+		return "TestDataClass [stringValue=" + stringValue + ",integerValue=" + integerValue + ", byteValue="
+				+ byteValue + ", floatValue=" + floatValue + "]";
+	}
+}
